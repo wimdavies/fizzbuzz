81c46fca19ca280e2b396974acce572fb4fd5360
Alice Wood <alicesophiewood@gmail.com>
2023-04-04 12:14:25 +0100

    added test for 9 and solved by implementing modulo for  3 to return 'fizz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(number)
-  if number == 3
+  if number % 3 == 0
     "fizz"
   else
     number
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,3 +11,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(4)).to eq 4
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+end
